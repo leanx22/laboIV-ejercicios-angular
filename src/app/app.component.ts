ddf9c9a690f24cacd920f570f6e7cf5d741327cd
Leandro <leandro.guia.dev@gmail.com>
2024-04-11 11:55:38 -0300

    Fin ejercicio 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  
   title = 'clase_1';
+  respuesta = 0;
+  protected num1: number = 0;
+  protected num2: number = 0;
+
+  public Suma()
+  {
+    this.respuesta =  this.num1 + this.num2; 
+  }
+
+  public Promedio()
+  {
+    this.respuesta =  (this.num1 + this.num2) / 2;
+  }
 }
